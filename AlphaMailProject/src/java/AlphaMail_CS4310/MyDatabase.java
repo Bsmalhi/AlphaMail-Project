3f1bdd8046897a32f054e696ced04b3127a8f9e3
Baljot Malhi <baljotjatt@gmail.com>
2016-07-07 21:45:35 -0700

    New changes

--- AlphaMailProject/src/java/AlphaMail_CS4310/MyDatabase.java
+++ AlphaMailProject/src/java/AlphaMail_CS4310/MyDatabase.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.willsuwei.cs3520;
+package AlphaMail_CS4310;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
